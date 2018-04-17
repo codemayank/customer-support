7ebe880a9b479ffb52de5926c477aba95d28e477
Mayank <mayankyadav14@gmail.com>
2018-04-17 23:09:11 +0530

    improve ui

--- client/client-app/components/query.component.js
+++ client/client-app/components/query.component.js
@@ -8,6 +8,8 @@
                 var vm = this;
                 vm.textLimit = 100;
                 vm.admin = false;
+                vm.username = window.localStorage.username;
+
                 vm.$onInit = function () {
                     console.log($routeParams);
                     var url = '/query/user/' + $routeParams.query_id;
@@ -19,70 +21,97 @@
                         .then(function (response) {
                             console.log(response.data);
                             vm.ticket = response.data.ticket;
+                            if(response.data.ticket.resolved){
+                                vm.disableMarkResolved = true;
+                            }
+                            if(response.data.ticket.status === 'Closed'){
+                                vm.disableCloseQuery = true;
+                            }
                         }).catch(function (error) {
                             console.log(error);
                         })
                 }
                 vm.submitMessage = function () {
+                    vm.disableSubmit = true;
                     var url = '/query/submit-message/' + vm.ticket._id;
-                    var message = { text: vm.messageForm.messageBody }
+                    var message = {};
+                    if(vm.action === 'newMessage'){
+                        message = { text: vm.messageForm.messageBody }
+                    }
                     var method = 'POST';
                     console.log(vm.ticket._id);
                     if (vm.action === 'edit') {
                         url = '/query/edit-message/' + vm.ticket._id + '/' + vm.selectedMessage._id;
-                        method = 'PUT',
-                            console.log(url, method);
+                        method = 'PUT';
+                        message = {text : vm.messageEditForm.messageBody}
+                        console.log(url, method);
                     }
                     queryService.submitData(url, method, message).then(function (response) {
                         console.log(response);
                         vm.ticket = response.data.ticket;
                         vm.messageForm = {}
                         vm.action === 'submit';
+                        vm.disableSubmit = false;
                     }).catch(function (error) {
                         console.log(error);
                         vm.messageForm = {}
+                        vm.disableSubmit = false;
                     })
                 }
 
                 vm.selectMessage = function (selectedMessage) {
-                    vm.messageForm = {};
+                    vm.messageEditForm = {};
                     console.log(selectedMessage);
                     vm.selectedMessage = selectedMessage;
-                    vm.messageForm.messageBody = selectedMessage.text;
+                    vm.messageEditForm.messageBody = selectedMessage.text;
                     vm.action = 'edit';
                 }
 
+                vm.newMessage = function(){
+                    vm.messageForm = {};
+                    vm.action = 'newMessage';
+                }
+
+
                 vm.deleteMessage = function(message){
+                    vm.disableDelete = true;
                     var url = '/query/delete-message/' + message._id;
                     console.log(url);
+                    var messageIndex = vm.ticket.messages.findIndex(x => x._id === message._id);
                     queryService.submitData(url, 'DELETE').then(function(response){
                         console.log(response);
+                        vm.ticket.messages.splice(messageIndex, 1);
                     }).catch(function(error){
                         console.log(error);
+                        vm.disableDelete = false;
                     })
                 }
 
                 vm.markResolved = function () {
+                    vm.disableMarkResolved = true;
                     var url = '/query/user/mark-resolved/' + vm.ticket._id;
                     queryService.submitData(url, 'PUT').then(function (response) {
                         console.log(response);
                         vm.ticket = response.data;
                     }).catch(function (response) {
                         console.log(error);
+                        vm.disableMarkResolved = false;
                     })
                 }
 
                 vm.closeQuery = function(){
+                    vm.disableCloseQuery = true;
                     var url = '/query/admin/close-query/' + vm.ticket._id;
                     queryService.submitData(url, 'PUT').then(function(response){
                         console.log(response);
                     }).catch(function(response){
                         console.log(error);
+                        vm.disableCloseQuery = false;
                     })
                 }
                 //TODO: provide a button for this function in the view.
                 vm.deleteTicket = function(){
-                    console.log(query);
+                    vm.disableDeleteTicket = true;
                     var url = '/query/user/delete-query/' + vm.ticket._id;
                     queryService.submitData(url, 'DELETE', null)
                         .then(function(response){
@@ -90,11 +119,11 @@
                             $location.path('/user/dashboard');
                         }).catch(function(error){
                             console.log(error);
+                            vm.disableDeleteTicket = false;
                         })
                 }
 
                 vm.checkResolved = function (resolved) {
-                    console.log(resolved)
                     if (resolved) {
                         console.log(resolved)
                         return 'badge badge-success badge-pill'
@@ -102,6 +131,7 @@
                         return 'badge badge-danger badge-pill'
                     }
                 }
+
 
             }
 
